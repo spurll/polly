94f2d91a5ad52fc5a020530b640d9daed6a36848
Brendan Curran-Johnson <brendan@bcjbcj.ca>
2013-07-19 16:58:13 -0500

    Base version, no internal alert system
    Stub version of the program that merely loops until a change occurs.

---
+++ polly.py
@@ -0,0 +1,41 @@
+from argparse import ArgumentParser
+from time import sleep
+
+from requests import get
+
+
+WAIT = 60
+
+
+def poll(address, delay=WAIT):
+    """
+    Poll a website, returning once the contents of the website have changed.
+    """
+    if '://' not in address:  # No protocol given, default to http
+        address = 'http://{}'.format(address)
+        
+    contents = get(address).text
+    
+    while True:
+        new_contents = get(address).text
+        
+        if contents != new_contents:
+            break
+            
+        wait(delay)
+        
+    return True
+
+def main():
+    parser = ArgumentParser(description="Poll a website for changes")
+    parser.add_argument('address', help="The address to poll"):
+    parser.add_argument('--delay', type=int, default=WAIT,
+                        help="Time (in seconds) to delay between polls.")
+    
+    args = parser.parse_args()
+    
+    poll(args.address, delay=args.delay)
+
+
+if __name__ == '__main__':
+    main()
